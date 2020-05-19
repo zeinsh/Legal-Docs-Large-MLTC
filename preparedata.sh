a50740eccfa50c1d8201a82029ab392b9e4e5142
Zein Shaheen <zeinshahine@gmail.com>
2020-05-20 00:36:22 +0300

    EuroVoc Graph Tool

--- preparedata.sh
+++ preparedata.sh
@@ -12,5 +12,9 @@
 #  tar  -C tmp/ -xzvf tmp/$filename
 #done < "$input"
 
+wget http://publications.europa.eu/resource/distribution/eurovoc/20190329-1/zip/eurovoc_dtd/eurovoc_xml.zip -O tmp/eurovoc_xml.zip
+unzip tmp/eurovoc_xml.zip -d tmp/EuroVoc
+python perpare_eurovoc.py
+
 python download_jrc_data.py --languages "en" --save_path "datasets/jrc_en_basic.csv"
 python download_jrc_data.py --languages "en,de,fr" --save_path "datasets/jrc_3langs_basic.csv"
