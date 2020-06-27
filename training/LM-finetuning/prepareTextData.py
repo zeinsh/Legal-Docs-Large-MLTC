90f6d4ea7fd6897ec281ed151e4be90ac68166bc
Zein <zeine.shahine@gmail.com>
2020-06-27 21:08:58 +0000

    LM-finetuning scrips are ready

--- training/LM-finetuning/prepareTextData.py
+++ training/LM-finetuning/prepareTextData.py
@@ -7,36 +7,38 @@ SPLIT_LABEL='split'
 TEXT_LABEL = 'text'
 TRAIN_TAG='train'
 CONCATENATATE_SYMBOL='\n'
-OUTPUT_PATH = './tmp/'
-
-
-TRAIN_OUTPUT_PATH = OUTPUT_PATH + 'train.txt'
-TEST_OUTPUT_PATH = OUTPUT_PATH + 'test.txt'
-
-Path(OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
-
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser("Prepare text data for LM finetuning")
     parser.add_argument("--dataset_path", help="path of the dataset", type=str)
+    parser.add_argument("--train_output_path", help="temporary text file to save training texts", type=str)
+    parser.add_argument("--test_output_path", help="temporary text file to save test texts", type=str)
 
     args = parser.parse_args()
 
     # args
     dataset_path =  args.dataset_path
+    train_output_path = args.train_output_path
+    test_output_path = args.test_output_path
+    
+    # prepare directories
+    Path(train_output_path).parent.mkdir(parents=True, exist_ok=True)
+    Path(test_output_path).parent.mkdir(parents=True, exist_ok=True)
 
+    # load data
     data=pd.read_csv(dataset_path)
 
+    # get train texts
     trainTexts = data[data[SPLIT_LABEL]==TRAIN_TAG][TEXT_LABEL]
     trainText = CONCATENATATE_SYMBOL.join(trainTexts)
 
+    # get validattion&test texts
     valTestTexts = data[~(data[SPLIT_LABEL]==TRAIN_TAG)][TEXT_LABEL]
     valTestText = CONCATENATATE_SYMBOL.join(valTestTexts)
 
-    with open(TRAIN_OUTPUT_PATH, 'w') as fout:
+    # save texts
+    with open(train_output_path, 'w') as fout:
         fout.write(trainText)
 
-    with open(TEST_OUTPUT_PATH, 'w') as fout:
+    with open(test_output_path, 'w') as fout:
         fout.write(valTestText)
-        
-    
